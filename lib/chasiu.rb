7a7f58369e9a4c3f63fb91949e3421b611bed078
Janice Wong <go.janicewong@gmail.com>
2017-08-17 22:15:21 -0400

    Update class method to search by keyword; add set_radius method

--- lib/chasiu.rb
+++ lib/chasiu.rb
@@ -1,10 +1,17 @@
 require 'Unirest'
-require 'dotenv-rails'
+require 'dotenv/load'
 
 class Chasiu
+  def initialize
+    @radius = 1000
+  end
+
   def self.list
-    response = Unirest.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=#{ENV["google_places_api_key"]}").body
+    response = Unirest.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.704751,-74.006027&radius=#{@radius}&type=restaurant&keyword=roast+pork&key=#{ENV["google_places_api_key"]}").body
     puts response
   end
-end
 
+  def self.set_radius(km)
+    @radius = km
+  end
+end
